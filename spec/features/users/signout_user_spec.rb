7006ae46e4db7cc4baf7fc989ae9a4a22ef172d7
Talal Talhouk <talal@sbcglobal.net>
2017-02-21 09:58:19 -0800

    add signout functionality

---
+++ spec/features/users/signout_user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "User Sign Out" do
+  before do
+    @user = User.create!(email: "bob@example.com", password:"password")
+    visit '/'
+
+    click_link "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+
+    click_button "Log in"
+  end
+  scenario "An signed in user signs out of app" do
+    visit '/'
+    click_link "Sign Out"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Signed out successfully.")
+    expect(page).to_not have_content("welcome: #{@user.email}")
+    expect(page).to_not have_link("Sign Out")
+
+  end
+
+end
