b0ba5425da27c87c2e703c4b97b0f87eca12447a
Talal Talhouk <talal@sbcglobal.net>
2017-02-21 15:21:00 -0800

    add show page and tests for exercise creation

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,9 +2,29 @@ class ExercisesController < ApplicationController
   def index
 
   end
+
+  def show
+    @exercise = Exercise.find(params[:id])
+  end
+
   def new
     @exercise = current_user.exercises.new
   end
   def create
+    @exercise = current_user.exercises.new(exercise_params)
+    if @exercise.save
+      flash[:notice] = 'Exercise has been created'
+      redirect_to user_exercise_path(current_user, @exercise)
+    else
+      flash[:danger] = 'Exercise could not be created'
+      render :new
+
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:duration, :workout, :workout_date)
   end
 end
