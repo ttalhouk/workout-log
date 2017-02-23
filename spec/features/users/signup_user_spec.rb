e52145324b1dcbcc4e0c735db6c10fe1eb595579
Talal Talhouk <talal@sbcglobal.net>
2017-02-22 16:14:06 -0800

    fix spec to check for invalid credentials

--- spec/features/users/signup_user_spec.rb
+++ spec/features/users/signup_user_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "User Sign up" do
     click_link "Sign Up"
 
     @email = "bob@example.com"
-    
+
     fill_in "First Name", with: "John"
     fill_in "Last Name", with: "Doe"
     fill_in "Email", with: @email
@@ -24,6 +24,25 @@ RSpec.feature "User Sign up" do
     expect(page).to have_link("Sign Out")
 
   end
+  scenario "A user signs up with invalid credentials" do
+    visit '/'
 
+    click_link "Sign Up"
+
+    @email = "bob@example.com"
+
+    fill_in "First Name", with: ""
+    fill_in "Last Name", with: ""
+    fill_in "Email", with: @email
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_button "Sign up"
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+
+
+  end
 
 end
